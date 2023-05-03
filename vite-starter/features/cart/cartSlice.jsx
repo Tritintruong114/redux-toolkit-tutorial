1e4d3b71c0e20343c94b2566de148c44c0704452
Tritintruong114 <truongtritin.bee@gmail.com>
2023-05-03 11:56:16 +0700

    this is for paractice async thunk

--- vite-starter/features/cart/cartSlice.jsx
+++ vite-starter/features/cart/cartSlice.jsx
@@ -1,12 +1,21 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import cartItems from "../../src/cartItems";
 
+const url = "https://course-api.com/react-useReducer-cart-project";
 const initialState = {
   cartItems: cartItems,
   amount: 4,
   total: 0,
   isLoading: true,
 };
+
+//
+
+export const getCartItems = createAsyncThunk("cart/getCartItems", () => {
+  return fetch(url)
+    .then((resp) => resp.json())
+    .catch((error) => console.log(error));
+});
 
 const cartSlice = createSlice({
   name: "cart",
@@ -43,6 +52,19 @@ const cartSlice = createSlice({
       state.total = total;
     },
     //
+  },
+  extraReducers: {
+    [getCartItems.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [getCartItems.fulfilled]: (state, action) => {
+      console.log(action);
+      state.isLoading = false;
+      state.cartItems = action.payload;
+    },
+    [getCartItems.rejected]: (state) => {
+      state.isLoading = false;
+    },
   },
 });
 
